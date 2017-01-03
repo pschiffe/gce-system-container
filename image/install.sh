9c5f2d46be04a2c9d65ee329bbe57c337d762fdd
Peter Schiffer <pschiffe@redhat.com>
2017-01-03 16:39:56 +0100

    Remove teardown service, add sysctl, udev and journald config

--- image/install.sh
+++ image/install.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 
 cp -a /etc/dhcp/dhclient.d/google_hostname.sh /HOST/etc/dhcp/dhclient.d/google_hostname.sh
 cp -a /usr/bin/set_hostname /HOST/usr/bin/set_hostname
+cp -a /etc/sysctl.d/11-gce-network-security.conf /HOST/etc/sysctl.d/11-gce-network-security.conf
+cp -a /etc/udev/rules.d/*gce*rules /HOST/etc/udev/rules.d/
+
+if ! grep -q '^ForwardToConsole=yes' /HOST/etc/systemd/journald.conf; then
+    echo 'ForwardToConsole=yes' >> /HOST/etc/systemd/journald.conf
+fi
 
 exit 0
 
