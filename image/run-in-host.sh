5554377ed3d41b1d7ae1fc8fe7ac3d3c5a0da8cb
Peter Schiffer <pschiffe@redhat.com>
2017-01-09 14:46:45 +0100

    Lastlog is not available on atomic host, let's ignore it

--- image/run-in-host.sh
+++ image/run-in-host.sh
@@ -2,12 +2,9 @@
 
 set -euo pipefail
 
-touch /var/log/lastlog
-
 mount --rbind /HOST/etc/ /etc/
 mount --rbind /HOST/home /home/
 mount --rbind /HOST/var/spool/mail /var/spool/mail
-mount --rbind /HOST/var/log/lastlog /var/log/lastlog
 mount --rbind /HOST/var/lib/google /var/lib/google
 
 exec $@
