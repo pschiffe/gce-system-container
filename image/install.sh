e0da8e86bb0d6dd1077b8b4bc18c53623b3c866e
Peter Schiffer <pschiffe@redhat.com>
2017-01-09 16:28:09 +0100

    /usr/local is writable on atomic host, we can use it for set_hostname script

--- image/install.sh
+++ image/install.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 cp -a /etc/dhcp/dhclient.d/google_hostname.sh /HOST/etc/dhcp/dhclient.d/google_hostname.sh
-cp -a /usr/bin/set_hostname /HOST/usr/bin/set_hostname
+cp -a /usr/bin/set_hostname /HOST/usr/local/bin/set_hostname
 cp -a /etc/sysctl.d/11-gce-network-security.conf /HOST/etc/sysctl.d/11-gce-network-security.conf
 cp -a /etc/udev/rules.d/*gce*rules /HOST/etc/udev/rules.d/
 
