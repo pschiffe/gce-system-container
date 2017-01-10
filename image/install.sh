6bd2222a4e0d7b57b14225a7625d2633579291be
Peter Schiffer <pschiffe@redhat.com>
2017-01-10 10:10:10 +0100

    Modify PATH var in /etc/NetworkManager/dispatcher.d/11-dhclient script

--- image/install.sh
+++ image/install.sh
@@ -7,6 +7,10 @@ cp -a /usr/bin/set_hostname /HOST/usr/local/bin/set_hostname
 cp -a /etc/sysctl.d/11-gce-network-security.conf /HOST/etc/sysctl.d/11-gce-network-security.conf
 cp -a /etc/udev/rules.d/*gce*rules /HOST/etc/udev/rules.d/
 
+if [ -f /HOST/etc/NetworkManager/dispatcher.d/11-dhclient ]; then
+    sed -i 's|^PATH=.*|PATH=/bin:/usr/bin:/sbin:/usr/local/bin|' /HOST/etc/NetworkManager/dispatcher.d/11-dhclient
+fi
+
 if ! grep -q '^ForwardToConsole=yes' /HOST/etc/systemd/journald.conf; then
     echo 'ForwardToConsole=yes' >> /HOST/etc/systemd/journald.conf
 fi
